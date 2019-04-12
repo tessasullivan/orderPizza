fd37510948454a05f3fe8f3c75c2b30845425885
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-12 11:58:30 -0700

    Initial README with specifications added along with constructors for Order and Pizza, Calculate cost of pizza based on size, crust type, and toppings, Contains back end logic to allow user to delete a pizza

---
+++ README.md
@@ -0,0 +1,55 @@
+# Pizza Order
+
+#### A program that allows you to order pizza
+
+#### By **Tessa Sullivan**
+
+## Description
+
+Customers can select the various toppings for a pizza.
+
+### Specs
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| Customer can select pizza size | Choice between small, medium, and large |Choice is saved |
+| Customer can select pizza crust | Choice between thin, regular, and Chicago style |Choice is saved|
+| Customer can select various meat toppings | Choose multiple toppings |Choice is saved|
+| Customer can select various cheese toppings | Choose multiple toppings |Choice is saved|
+| Customer can select various veggie toppings | Choose multiple toppings |Choice is saved|
+| Customer can select various sauce toppings | Choose multiple toppings |Choice is saved|
+| Customer can select various meat toppings | Choose multiple toppings |Choice is saved|
+| Customer ends customization of pizza | Order pizza is selected |Customer receives a summary of his choices and the total |
+| Customer can order more than one pizza | A second order is filled out | Both pizzas are provided as links |
+| | | |
+| | | |
+| | | |
+| | | |
+| | | |
+| | | |
+
+## Setup/Installation Requirements
+
+1. Clone this repository.
+2. Open your browser and navigate to index.html
+3. Enjoy!
+
+## Known Bugs
+* No known bugs at this time.
+
+## Technologies Used
+
+* HTML
+* Javascript
+  * jQuery
+* CSS
+  * Bootstrap
+
+## Support and contact details
+
+_Contact Tessa Sullivan @ tessa.sullivan@gmail.com or Marissa Perry @ ohthatmarissa@gmail.com_
+
+### License
+
+*{This software is licensed under the MIT license}*
+
+Copyright (c) 2019 **_{Marissa Perry and Tessa Sullivan}_**
---
+++ js/scripts.js
@@ -0,0 +1,135 @@
+//////////////////
+//Business logic
+
+// Order constructor
+function Order() {
+  this.pizzas = [];
+  this.currentId = 0
+}
+
+Order.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
+}
+
+Order.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+Order.prototype.findPizza = function(id) {
+  for (var i=0; i < this.pizzas.length; i++) {
+    if (this.pizzas[i]) {
+      if (this.pizzas[i].id === id) {
+        return this.pizzas[i];
+      }
+    }
+  }
+  return false;
+}
+
+Order.prototype.deletePizza = function(id) {
+  for (var i=0; i < this.pizzas.length; i++) {
+    if (this.pizzas[i]) {
+      if (this.pizzas[i].id === id) {
+//        delete this.pizzas[i];
+          this.pizzas.splice(i,1);
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Pizza constructor
+function Pizza(size, crust, sauce, meatToppings, veggieToppings, cheeseToppings) {
+  this.size = size;
+  this.crust = crust;
+  this.sauce = sauce;
+  this.meatToppings = meatToppings;
+  this.veggieToppings = veggieToppings;
+  this.cheeseToppings = cheeseToppings;
+}
+
+// Calculate cost based on size
+// Chicago style pizza is $2 more than the other crust types
+// Small is $10, Medium is $13, Large is $18
+// Meat toppings are $2 for small, $2.50 for medium and $3.50 for Large
+// Veggie toppings are $1 for small, $1.50 for medium and $2 for Large
+// Cheese toppings - 1 is included for free but
+// $1 for additional for small, $1.50 for medium, and $2 for Large
+Pizza.prototype.calculateCost = function() {
+  var cost = 0;
+
+  switch (size) {
+    case "small":
+      cost += 10;
+      cost += meatToppings.length*2;
+      cost += veggieToppings.length;
+      cost += cheeseToppings.length;
+      cost -= 1; // for the free cheese topping
+      break;
+
+    case "medium":
+      cost += 13;
+      cost += meatToppings.length*2.5;
+      cost += veggieToppings.length*1.5;
+      cost += cheeseToppings.length*1.5;
+      cost -= 1.5; // for the free cheese topping
+      break;
+
+    case "large":
+      cost += 18;
+      cost += meatToppings.length*3.5;
+      cost += veggieToppings.length*2;
+      cost += cheeseToppings.length*2;
+      cost -= 2; // for the free cheese topping
+      break;
+
+  }
+  // crust
+  if (this.crust === "Chicago") {
+    cost += 2;
+  }
+  return cost;
+}
+
+var pizza1 = new Pizza (
+  size = "small",
+  crust = 'Chicago',
+  sauce = 'pizza',
+  meatToppings = ['Canadian Bacon', 'Sausage'],
+  veggieToppings = ['Black olives', 'Spinach', 'Tomatoes'],
+  cheeseToppings = ['Mozzarella', 'Feta'],
+)
+
+var pizza2 = new Pizza (
+  size = "medium",
+  crust = 'Chicago',
+  sauce = 'pizza',
+  meatToppings = ['Canadian Bacon', 'Sausage'],
+  veggieToppings = ['Black olives', 'Spinach', 'Tomatoes'],
+  cheeseToppings = ['Mozzarella', 'Feta'],
+)
+
+var pizzaCost = pizza1.calculateCost();
+var order = new Order();
+order.addPizza(pizza1);
+order.addPizza(pizza2);
+
+// var foundPizza = order.findPizza(1);
+console.log(order);
+//order.deletePizza(1);
+console.log(order);
+
+////////////////////////////////////////////
+// User logic
+
+
+
+$().ready(function() {
+
+  $('button#roll').click(function() {
+
+  });
+});
